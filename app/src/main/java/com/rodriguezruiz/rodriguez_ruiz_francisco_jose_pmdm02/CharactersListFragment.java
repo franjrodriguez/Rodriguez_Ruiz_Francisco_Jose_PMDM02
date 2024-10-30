5bda9382cc21ebcf49fc931481cd536b3727205d
Fran <fjose.nova@icloud.com>
2024-10-30 19:26:56 +0100

    Se ha añadido la lista de personajes con su informacion a string.xml y se termina (en principio) el fragment de la lista de personajes

--- app/src/main/java/com/rodriguezruiz/rodriguez_ruiz_francisco_jose_pmdm02/CharactersListFragment.java
+++ app/src/main/java/com/rodriguezruiz/rodriguez_ruiz_francisco_jose_pmdm02/CharactersListFragment.java
@@ -2,63 +2,172 @@ package com.rodriguezruiz.rodriguez_ruiz_francisco_jose_pmdm02;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
+import com.rodriguezruiz.rodriguez_ruiz_francisco_jose_pmdm02.databinding.FragmentCharactersListBinding;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link CharactersListFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+
 public class CharactersListFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public CharactersListFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment CharactersListFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static CharactersListFragment newInstance(String param1, String param2) {
-        CharactersListFragment fragment = new CharactersListFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private FragmentCharactersListBinding binding;
+    private ArrayList<CharacterData> character;
+    private GameRecyclerViewAdapter adapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_characters_list, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        loadCharacters();
+
+        adapter = new GameRecyclerViewAdapter(character, getActivity());
+        binding.charactersRecyclerview.setLayoutManager(new LinearLayoutManager(getContext()));
+        binding.charactersRecyclerview.setAdapter(adapter);
+    }
+
+    private void loadCharacters() {
+        ArrayList<CharacterData> characters = new ArrayList<>();
+
+        characters.add(new CharacterData(
+                R.drawable.foto1r,
+                getString(R.string.mario_name),
+                getString(R.string.mario_description),
+                getString(R.string.mario_skills),
+                getString(R.string.mario_detail)
+        ));
+
+        characters.add(new CharacterData(
+                R.drawable.foto2r,
+                getString(R.string.luigi_name),
+                getString(R.string.luigi_description),
+                getString(R.string.luigi_skills),
+                getString(R.string.luigi_detail)
+        ));
+
+        characters.add(new CharacterData(
+                R.drawable.foto3r,
+                getString(R.string.peach_name),
+                getString(R.string.peach_description),
+                getString(R.string.peach_skills),
+                getString(R.string.peach_detail)
+        ));
+
+        characters.add(new CharacterData(
+                R.drawable.foto4r,
+                getString(R.string.bowser_name),
+                getString(R.string.bowser_description),
+                getString(R.string.bowser_skills),
+                getString(R.string.bowser_detail)
+        ));
+
+        characters.add(new CharacterData(
+                R.drawable.foto5r,
+                getString(R.string.yoshi_name),
+                getString(R.string.yoshi_description),
+                getString(R.string.yoshi_skills),
+                getString(R.string.yoshi_detail)
+        ));
+
+        characters.add(new CharacterData(
+                R.drawable.foto6r,
+                getString(R.string.toad_name),
+                getString(R.string.toad_description),
+                getString(R.string.toad_skills),
+                getString(R.string.toad_detail)
+        ));
+
+        characters.add(new CharacterData(
+                R.drawable.foto7r,
+                getString(R.string.wario_name),
+                getString(R.string.wario_description),
+                getString(R.string.wario_skills),
+                getString(R.string.wario_detail)
+        ));
+
+        characters.add(new CharacterData(
+                R.drawable.foto8r,
+                getString(R.string.donkey_kong_name),
+                getString(R.string.donkey_kong_description),
+                getString(R.string.donkey_kong_skills),
+                getString(R.string.donkey_kong_detail)
+        ));
+
+        characters.add(new CharacterData(
+                R.drawable.foto9r,
+                getString(R.string.koopa_troopa_name),
+                getString(R.string.koopa_troopa_description),
+                getString(R.string.koopa_troopa_skills),
+                getString(R.string.koopa_troopa_detail)
+        ));
+
+        characters.add(new CharacterData(
+                R.drawable.foto10r,
+                getString(R.string.goomba_name),
+                getString(R.string.goomba_description),
+                getString(R.string.goomba_skills),
+                getString(R.string.goomba_detail)
+        ));
+
+        characters.add(new CharacterData(
+                R.drawable.foto11r,
+                getString(R.string.daisy_name),
+                getString(R.string.daisy_description),
+                getString(R.string.daisy_skills),
+                getString(R.string.daisy_detail)
+        ));
+
+        characters.add(new CharacterData(
+                R.drawable.foto12r,
+                getString(R.string.rosalina_name),
+                getString(R.string.rosalina_description),
+                getString(R.string.rosalina_skills),
+                getString(R.string.rosalina_detail)
+        ));
+
+        characters.add(new CharacterData(
+                R.drawable.foto13r,
+                getString(R.string.toadette_name),
+                getString(R.string.toadette_description),
+                getString(R.string.toadette_skills),
+                getString(R.string.toadette_detail)
+        ));
+
+        characters.add(new CharacterData(
+                R.drawable.foto14r,
+                getString(R.string.waluigi_name),
+                getString(R.string.waluigi_description),
+                getString(R.string.waluigi_skills),
+                getString(R.string.waluigi_detail)
+        ));
+
+        characters.add(new CharacterData(
+                R.drawable.foto15r,
+                getString(R.string.kamek_name),
+                getString(R.string.kamek_description),
+                getString(R.string.kamek_skills),
+                getString(R.string.kamek_detail)
+        ));
+
+    }
+
+    @Override
+    public void onStart() {
+
+        super.onStart();
+        if (getActivity() != null) {
+            ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle(R.string.title_app);
+        }
     }
 }
---
+++ app/src/main/java/com/rodriguezruiz/rodriguez_ruiz_francisco_jose_pmdm02/GameRecyclerViewAdapter.java
@@ -0,0 +1,30 @@
+package com.rodriguezruiz.rodriguez_ruiz_francisco_jose_pmdm02;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class GameRecyclerViewAdapter extends RecyclerView.Adapter {
+    public GameRecyclerViewAdapter(ArrayList<CharacterData> character, FragmentActivity activity) {
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
